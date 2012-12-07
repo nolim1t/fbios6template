cbbdb88d58d8296c4dbdcdd1c926973395d0e98f
@nolim1t <barry.teoh+github@gmail.com>
2012-12-07 11:48:19 +1100

    Updated gitignore

---
+++ ParseFBTemplate/ParseFBTemplate/AppConstants.h
@@ -0,0 +1,13 @@
+//
+//  AppConstants.h
+//  ParseFBTemplate
+//
+//  Created by Barry Teoh on 12/7/12.
+//  Copyright (c) 2012 perceptionz.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppConstants : NSObject
+
+@end
---
+++ ParseFBTemplate/ParseFBTemplate/AppConstants.m
@@ -0,0 +1,13 @@
+//
+//  AppConstants.m
+//  ParseFBTemplate
+//
+//  Created by Barry Teoh on 12/7/12.
+//  Copyright (c) 2012 perceptionz.net. All rights reserved.
+//
+
+#import "AppConstants.h"
+
+@implementation AppConstants
+
+@end
