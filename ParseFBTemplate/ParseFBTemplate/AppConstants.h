0727d86df311b77b32099f998407676c78d954b0
@nolim1t <barry.teoh+github@gmail.com>
2012-12-07 11:57:48 +1100

    Remove AppConstants

--- ParseFBTemplate/ParseFBTemplate/AppConstants.h
+++
@@ -1,5 +0,0 @@
-// App Configuration
-
-#define kParseAppId @"EkTvECsuMqL50QnZErbyOC7kRohZsqjtgelFGNe1"
-#define kParseClientKey @"E9dWecXT6QFszEwZlYZ4qQIvYGp3kbtP1hr2tILH"
-#define kFacebookAppId @"251996114837587"
